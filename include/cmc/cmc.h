9798a7929bbeb948628d04e2290d46dbeb2b2514
elenagonzalez870 <elenagonzalez870@gmail.com>
2022-11-16 17:21:48 -0600

    Adding files for additional output file

--- include/cmc/cmc.h
+++ include/cmc/cmc.h
@@ -1081,6 +1081,11 @@ typedef struct{
  * @brief Write out information about neutron stars (0=off, 1=on)
  * */
         int WRITE_MOREPULSAR_INFO;
+#define WRITE_MORECOLL_INFO "Write out information about stellar collisions (0=off, 1=on)"
+/**
+ *  * @brief Write out information about stellar collisions(0=off, 1=on)
+ *   * */
+        int WRITE_MORECOLL_INFO;
 #define PARAMDOC_BHNS_TDE "Treat BH(NS)--MS TDEs in TDE vs direct collision limit (1=TDE, 0=coll)"
 /**
  * @brief Treat BH(NS)--MS TDEs in TDE vs direct collision limit (1=TDE, 0=coll)
