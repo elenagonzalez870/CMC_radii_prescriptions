1a78d57e66a7a234686e3016da2a85da4d879638
elenagonzalez870 <elenagonzalez870@gmail.com>
2022-11-24 20:48:55 -0600

    additional changes for output file

--- src/cmc/cmc_sscollision.c
+++ src/cmc/cmc_sscollision.c
@@ -257,14 +257,21 @@ void sscollision_do(long k, long kp, double rperimax, double w[4], double W, dou
                                 star_r[get_global_idx(knew)], star[knew].se_k, star[k].se_k, star[kp].se_k,
 				star[k].rad*units.l/RSUN, star[kp].rad*units.l/RSUN,
                     b*units.l/RSUN,W*units.l/units.t/1.e5, rperi*units.l/RSUN, collisions_multiple_hold);
+			/* units should be okay already */
+			double rho0_c = (star[k].se_mc  ) / ((4/3)* PI * pow((star[k].se_rc  ),3));
+			double rho1_c = (star[kp].se_mc ) / ((4/3)* PI * pow((star[kp].se_rc ),3));
+			double rho0_env = (star[k].se_menv  ) / ((4/3)* PI * pow((star[k].se_renv ),3));
+			double rho1_env = (star[kp].se_menv  ) / ((4/3)* PI *pow((star[kp].se_renv  ),3));
+			double rhor_c = (star[knew].se_mc ) / ((4/3)* PI *pow((star[knew].se_rc ),3));
+			double rhor_env = (star[knew].se_menv ) / ((4/3)* PI * pow((star[knew].se_renv ),3));
 
-			double rho0_c = (star[k].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_rc * units.l / RSUN ),3));
-			double rho1_c = (star[kp].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[kp].se_rc * units.l / RSUN ),3));
-			double rho0_env = (star[k].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_renv * units.l / RSUN ),3));
-			double rho1_env = (star[kp].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[kp].se_renv * units.l / RSUN ),3));
-			double rhor_c = (star[knew].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[knew].se_rc * units.l / RSUN ),3));
-			double rhor_env = (star[knew].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[knew].se_renv * units.l / RSUN ),3));
-
+			if(isnan(rho0_c)){rho0_c = -100;}
+			if(isnan(rho1_c)){rho1_c = -100;}
+			if(isnan(rhor_c)){rhor_c = -100;}
+			if(isnan(rho0_env)){rho0_env = -100;}
+			if(isnan(rho1_env)){rho1_env = -100;}
+			if(isnan(rhor_env)){rhor_env = -100;}
+			
 			/*Elena: Creating a file with additional collision information */
 			parafprintf(morecollfile, "%g single-single %ld %ld %g %g %g %g %g %g %g %g %d %d %ld %g %g %g %g %d %g %g\n",
 				    TotalTime, star[k].id, star[kp].id, mass_k * units.mstar / FB_CONST_MSUN, mass_kp * units.mstar / FB_CONST_MSUN,
@@ -449,13 +456,21 @@ void sscollision_do(long k, long kp, double rperimax, double w[4], double W, dou
                     b*units.l/RSUN,W*units.l/units.t/1.e5,  rperi*units.l/RSUN, collisions_multiple_hold);
 
 			/*Elena: Creating a file with additional collision information */
-			double rho0_c = (star[k].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_rc * units.l / RSUN ),3));
-			double rho1_c = (star[kp].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[kp].se_rc * units.l / RSUN ),3));
-			double rho0_env = (star[k].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_renv * units.l / RSUN ),3));
-			double rho1_env = (star[kp].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[kp].se_renv * units.l / RSUN ),3));
-			double rhor_c = (star[knew].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[knew].se_rc * units.l / RSUN ),3));
-			double rhor_env =(star[knew].se_menv * units.mstar / FB_CONST_MSUN ) /((4/3)* PI * pow((star[knew].se_renv * units.l/RSUN ),3));
-
+			double rho0_c = (star[k].se_mc ) / ((4/3)* PI * pow((star[k].se_rc ),3));
+			double rho1_c = (star[kp].se_mc ) / ((4/3)* PI * pow((star[kp].se_rc ),3));
+			double rho0_env = (star[k].se_menv ) / ((4/3)* PI * pow((star[k].se_renv ),3));
+			double rho1_env = (star[kp].se_menv ) / ((4/3)* PI *pow((star[kp].se_renv ),3));
+			double rhor_c = (star[knew].se_mc ) / ((4/3)* PI *pow((star[knew].se_rc ),3));
+			double rhor_env =(star[knew].se_menv ) /((4/3)* PI * pow((star[knew].se_renv ),3));
+			
+			
+			if(isnan(rho0_c)){rho0_c = -100;}
+			if(isnan(rho1_c)){rho1_c = -100;}
+			if(isnan(rhor_c)){rhor_c = -100;}
+			if(isnan(rho0_env)){rho0_env = -100;}
+			if(isnan(rho1_env)){rho1_env = -100;}
+			if(isnan(rhor_env)){rhor_env = -100;}
+	
 			parafprintf(morecollfile, "%g single-single %ld %ld %g %g %g %g %g %g %g %g %d %d %ld %g %g %g %g %d %g %g\n",
 				    TotalTime, star[k].id, star[kp].id, mass_k * units.mstar / FB_CONST_MSUN, mass_kp * units.mstar / FB_CONST_MSUN,
 				    star[k].rad*units.l/RSUN, star[kp].rad*units.l/RSUN,rho0_c,rho1_c,rho0_env, rho1_env, star[k].se_k, star[kp].se_k,
@@ -615,13 +630,20 @@ void sscollision_do(long k, long kp, double rperimax, double w[4], double W, dou
                     b*units.l/RSUN,W*units.l/units.t/1.e5, rperi*units.l/RSUN, collisions_multiple_hold);
 
 			/*Elena: Creating a file with additional collision information */
-			double rho0_c = (star[k].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_rc * units.l / RSUN ),3));
-			double rho1_c = (star[kp].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[kp].se_rc * units.l / RSUN ),3));
-			double rho0_env = (star[k].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_renv * units.l / RSUN ),3));
-			double rho1_env = (star[kp].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[kp].se_renv * units.l / RSUN ),3));
-			double rhor_c = (star[knew].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[knew].se_rc * units.l / RSUN ),3));
-			double rhor_env = (star[knew].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[knew].se_renv * units.l / RSUN ),3));
+			double rho0_c = (star[k].se_mc ) / ((4/3)* PI * pow((star[k].se_rc),3));
+			double rho1_c = (star[kp].se_mc ) / ((4/3)* PI * pow((star[kp].se_rc ),3));
+			double rho0_env = (star[k].se_menv ) / ((4/3)* PI * pow((star[k].se_renv ),3));
+			double rho1_env = (star[kp].se_menv ) / ((4/3)* PI *pow((star[kp].se_renv ),3));
+			double rhor_c = (star[knew].se_mc ) / ((4/3)* PI *pow((star[knew].se_rc ),3));
+			double rhor_env = (star[knew].se_menv ) / ((4/3)* PI * pow((star[knew].se_renv),3));
 
+			if(isnan(rho0_c)){rho0_c = -100;}
+			if(isnan(rho1_c)){rho1_c = -100;}
+			if(isnan(rhor_c)){rhor_c = -100;}
+			if(isnan(rho0_env)){rho0_env = -100;}
+			if(isnan(rho1_env)){rho1_env = -100;}
+			if(isnan(rhor_env)){rhor_env = -100;}
+			
 			parafprintf(morecollfile, "%g single-single %ld %ld %g %g %g %g %g %g %g %g %d %d %ld %g %g %g %g %d %g %g\n",
 				    TotalTime, star[k].id, star[kp].id, mass_k * units.mstar / FB_CONST_MSUN, mass_kp * units.mstar / FB_CONST_MSUN,
 				    star[k].rad*units.l/RSUN, star[kp].rad*units.l/RSUN,rho0_c,rho1_c,rho0_env, rho1_env, star[k].se_k, star[kp].se_k,
@@ -824,17 +846,24 @@ void sscollision_do(long k, long kp, double rperimax, double w[4], double W, dou
             b*units.l/RSUN,W*units.l/units.t/1.e5, rperi*units.l/RSUN, collisions_multiple);
 
 		/*Elena: Creating a file with additional collision information */
-			double rho0_c = (star[k].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_rc * units.l / RSUN ),3));
-			double rho1_c = (star[kp].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[kp].se_rc * units.l / RSUN ),3));
-			double rho0_env = (star[k].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_renv * units.l / RSUN ),3));
-			double rho1_env = (star[kp].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[kp].se_renv * units.l / RSUN ),3));
-			double rhor_c = (star[knew].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[knew].se_rc * units.l / RSUN ),3));
-			double rhor_env = (star[knew].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[knew].se_renv * units.l / RSUN ),3));
+			double rho0_c = (star[k].se_mc) / ((4/3)* PI * pow((star[k].se_rc),3));
+			double rho1_c = (star[kp].se_mc) / ((4/3)* PI * pow((star[kp].se_rc ),3));
+			double rho0_env = (star[k].se_menv ) / ((4/3)* PI * pow((star[k].se_renv ),3));
+			double rho1_env = (star[kp].se_menv ) / ((4/3)* PI *pow((star[kp].se_renv ),3));
+			double rhor_c = (star[knew].se_mc ) / ((4/3)* PI *pow((star[knew].se_rc ),3));
+			double rhor_env = (star[knew].se_menv) / ((4/3)* PI * pow((star[knew].se_renv ),3));
 
+			if(isnan(rho0_c)){rho0_c = -100;}
+			if(isnan(rho1_c)){rho1_c = -100;}
+			if(isnan(rhor_c)){rhor_c = -100;}
+			if(isnan(rho0_env)){rho0_env = -100;}
+			if(isnan(rho1_env)){rho1_env = -100;}
+			if(isnan(rhor_env)){rhor_env = -100;}
+			
 			parafprintf(morecollfile, "%g single-single %ld %ld %g %g %g %g %g %g %g %g %d %d %ld %g %g %g %g %d %g %g\n",
 				    TotalTime, star[k].id, star[kp].id, mass_k * units.mstar / FB_CONST_MSUN, mass_kp * units.mstar / FB_CONST_MSUN,
 				    star[k].rad*units.l/RSUN, star[kp].rad*units.l/RSUN,rho0_c,rho1_c,rho0_env, rho1_env, star[k].se_k, star[kp].se_k,
-				    star[knew].id, star_m[get_global_idx(knew)] * units.mstar / FB_CONST_MSUN, star[knew].rad*units.l/RSUN, rhor_c, rhor_env, star[knew].se_k, W*units.l/units.t/1.e5, rperi*units.l/RSUN);
+				    star[knew].id, star_m[get_global_idx(knew)] * units.mstar / FB_CONST_MSUN, star[knew].rad*units.l/RSUN, rhor_c, 					    rhor_env, star[knew].se_k, W*units.l/units.t/1.e5, rperi*units.l/RSUN);
 
                 /* destroy two progenitors */
                 destroy_obj(k);
@@ -939,13 +968,20 @@ void sscollision_do(long k, long kp, double rperimax, double w[4], double W, dou
 
 				/*Elena: Creating a file with additional collision information */
 				
-			double rho0_c = (star[k].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_rc * units.l / RSUN ),3));
-			double rho1_c = (star[kp].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[kp].se_rc * units.l / RSUN ),3));
-			double rho0_env = (star[k].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[k].se_renv * units.l / RSUN ),3));
-			double rho1_env = (star[kp].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[kp].se_renv * units.l / RSUN ),3));
-			double rhor_c = (star[knew].se_mc * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI *pow((star[knew].se_rc * units.l / RSUN ),3));
-			double rhor_env = (star[knew].se_menv * units.mstar / FB_CONST_MSUN ) / ((4/3)* PI * pow((star[knew].se_renv * units.l / RSUN ),3));
+			double rho0_c   = (star[k].se_mc)      / ((4/3)* PI * pow((star[k].se_rc),3));
+			double rho1_c   = (star[kp].se_mc)     / ((4/3)* PI * pow((star[kp].se_rc),3));
+			double rho0_env = (star[k].se_menv)    / ((4/3)* PI * pow((star[k].se_renv),3));
+			double rho1_env = (star[kp].se_menv)   / ((4/3)* PI * pow((star[kp].se_renv),3));
+			double rhor_c   = (star[knew].se_mc)   / ((4/3)* PI * pow((star[knew].se_rc),3));
+			double rhor_env = (star[knew].se_menv) / ((4/3)* PI * pow((star[knew].se_renv),3));
 	
+			if(isnan(rho0_c)){rho0_c = -100;}
+			if(isnan(rho1_c)){rho1_c = -100;}
+			if(isnan(rhor_c)){rhor_c = -100;}
+			if(isnan(rho0_env)){rho0_env = -100;}
+			if(isnan(rho1_env)){rho1_env = -100;}
+			if(isnan(rhor_env)){rhor_env = -100;}
+			
 			parafprintf(morecollfile, "%g single-single %ld %ld %g %g %g %g %g %g %g %g %d %d %ld %g %g %g %g %d %g %g\n",
 				    TotalTime, star[k].id, star[kp].id, mass_k * units.mstar / FB_CONST_MSUN, mass_kp * units.mstar / FB_CONST_MSUN,
 				    star[k].rad*units.l/RSUN, star[kp].rad*units.l/RSUN,rho0_c,rho1_c,rho0_env, rho1_env, star[k].se_k, star[kp].se_k,
